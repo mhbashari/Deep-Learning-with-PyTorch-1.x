b814dbd48a80d22558d14ad6cf7a3ea5f8f20b7e
basalam1742 <mhbashari@gmail.com>
2022-07-11 05:10:13 +0430

    some refinement

---
+++ temp.py
@@ -0,0 +1,15 @@
+import torch
+
+# https://ruder.io/optimizing-gradient-descent/
+
+# torch.optim.Adadelta
+# torch.optim.Adam
+# torch.optim.Adagrad
+# torch.optim.Adamax
+# torch.optim.AdamW
+# torch.optim.ASGD
+# torch.optim.LBFGS
+# torch.optim.RMSprop
+# torch.optim.Rprop
+# torch.optim.SGD
+# torch.optim.SparseAdam
